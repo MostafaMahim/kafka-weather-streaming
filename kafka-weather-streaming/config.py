bb4faf7fc4e9624f75a381d1d4099f2a6f5923b1
mostafa <mostafamahimakash384@gmail.com>
2026-05-22 12:18:14 -0400

    Add Kafka config and gitignore

---
+++ kafka-weather-streaming/config.py
@@ -0,0 +1,10 @@
+KAFKA_CONFIG = {
+    'bootstrap.servers': 'pkc-921jm.us-east-2.aws.confluent.cloud:9092',
+    'sasl.mechanisms': 'PLAIN',
+    'security.protocol': 'SASL_SSL',
+    'sasl.username': 'X6HKKB75XGJAPA7W',
+    'sasl.password': 'cfltaf86OJKqDQEaa7qvpmCnr/rlZ+ymmp4S/4ySaWE6em5Ey9NWYFTv15SfaFJg',
+}
+
+RAW_TOPIC = 'raw-data'
+PREDICTIONS_TOPIC = 'predictions'
